891927a6c54d09220f5ba73330f0028a83ab8482
isaskeee <isaque.rod393@gmail.com>
2024-10-02 19:49:45 -0300

    area kids e pet finalizado isaque

--- js/script.js
+++ js/script.js
@@ -23,3 +23,35 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(section); // Observa cada seção
     });
 });
+
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('fhpopup').classList.add('show');
+    }, 4000);
+};
+
+document.getElementById('fhclose-btn').onclick = function() {
+    document.getElementById('fhpopup').classList.remove('show');
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.section');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const info = entry.target.querySelector('.info');
+            if (entry.isIntersecting) {
+                info.classList.add('visible');
+            } else {
+                info.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.7 // Quando 50% do elemento estiver visível
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
